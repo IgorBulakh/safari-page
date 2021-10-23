97fff0b82f3c9340f011923f8f06dc26d2eb3f16
IgorBulakh <bulach.igor@gmail.com>
2021-10-23 23:03:53 +0300

    add block slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
-$(function(){
+$(function () {
+  $(".popup-link").magnificPopup({
+    disableOn: 700,
+    type: "iframe",
+    mainClass: "mfp-fade",
+    removalDelay: 160,
+    preloader: false,
 
+    fixedContentPos: false,
+  });
+
+  $(".gallery__slider").slick();
+
+  $(".gallery__item-inner").magnificPopup({
+    delegate: "a",
+    type: "image",
+    tLoading: "Loading image #%curr%...",
+    mainClass: "mfp-img-mobile",
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
+    },
+  });
 });
